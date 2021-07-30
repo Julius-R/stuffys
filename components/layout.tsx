b80ec4b8725f12e497145d9babc7dd5bae147129
Julius-R <julius.robinson95@yahoo.com>
2021-07-29 18:50:16 -0600

    structured the boilerplate for landing page

--- components/layout.tsx
+++ components/layout.tsx
@@ -39,7 +39,7 @@ const Layout = ({ title, children }: IProps) => {
 			<footer className="text-gray-600 body-font">
 				<div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
 					<p className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-						<span className="ml-3 text-xl">Stuffys</span>
+						<span className="ml-3 text-xl">Stuffs</span>
 					</p>
 					<p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
 						Proudly made by —
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
+import Hero from "../components/Hero";
 import Layout from "../components/layout";
+import MiniGallery from "../components/MiniGallery";
 
 export default function Home() {
 	return (
-		<Layout title="Stuffs  | Yup, we're a store!">
-			<h1>Home</h1>
+		<Layout title="Stuffs | Yup, we're a store!">
+			<Hero />
+			<MiniGallery />
 		</Layout>
 	);
 }
